cd26de087ce4e6ba8693729208de4ebb4d6a3110
Zubair Bashir <zubair.bashirzubair@gmail.com>
2021-02-02 18:13:45 +0530

    Add documentation

--- singleton-pattern.js
+++ singleton-pattern.js
@@ -1,5 +1,11 @@
 /**
  * * THE SINGLETON PATTERN
+ * 
+ * ! Lazy initialisation of the singleton instance is the key factor that differentiates it from the static instances.
+ * 
+ * * Applicability of a singleton instance:
+ * ?1 When a single instance with a global access point is need acroos multiple clients.
+ * ?2 When an instance is to be extensible and extended instance must be used by clients without changing the client code(Modify the singleton manager method to behave like a factory).
  */
 
 const serverContext = (function(){
@@ -47,19 +53,53 @@ context.add("admin", "Jhon Doe");
 context.get("admin");
 
 
-//singleton factory //does it make sense?
+/**
+ * ? SINGLETON FACTORY
+ * 
+ * ! Supplier Classes don't have to be Singletons by themselves. 
+ * ! Let the application be the singleton, cause the singleton behaviour is the application's need.
+ * ! This allows Supplier classes to be separately instantiable.
+ * ! Now we could create mock instances for use with unit testing.
+ * ! The application needs a singleton manager.
+ * ! We could build a HOF that wraps a constructor into a Singleton(Manager)
+ * 
+ * ?Does it make sense?
+ * 
+ * 
+ */
+
+/**
+ * * A singleton manager for a given constructor
+ */
 function SingletonWrapper(Constructor){
+  //store the instance reference on Consrtuctor to avoid exposing it
   this.getInstance = () => {
+    //reference is available via closure of getInstance() over Constructor
     if(!Constructor.instance){
       Constructor.instance = new Constructor();
     }
     return Constructor.instance;
-  }
-  
+  } 
 }
 
-const SomeSingletonManager = new SingletonWrapper(function(){this.a = 2;});
-const OtherSingletonManager = new SingletonWrapper(function(){this.b = 2;});
+/**
+ * * A demo constructor
+ */
+function Car(){
+  this.make = "Honda";
+  this.year = 2020;
+}
 
-SomeSingletonManager.getInstance();
-OtherSingletonManager.getInstance();
+/**
+ * * Singleton manager for Car constructor
+ * * Pass around into the clients.
+ */
+const CarSingletonManager = new SingletonWrapper(Car);
+
+/**
+ * * Car is still instantiable.
+ */
+const mockCar = new Car();
+
+CarSingletonManager.getInstance();
+
